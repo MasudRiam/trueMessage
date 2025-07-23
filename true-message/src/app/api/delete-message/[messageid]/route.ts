15c91898079983445c1c932aea2c13a53222e6ce
MasudRiam <masudp.riam@gmail.com>
2025-07-23 20:39:44 +0600

    Ready to deploy

--- true-message/src/app/api/delete-message/[messageid]/route.ts
+++ true-message/src/app/api/delete-message/[messageid]/route.ts
@@ -1,28 +1,26 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/option";
-import UserModel, { User } from "@/model/User";
+import UserModel from "@/model/User";
 import dbConnect from "@/lib/dbConnect";
 import { NextRequest } from "next/server";
+import { User } from "next-auth";
+
+
 
 export async function DELETE(
   request: NextRequest,
-  context: { params: { messageid: string } }
+   context: any
 ) {
-
-    const params = await context.params;
-    const messageId = params.messageid;
+  const messageid = context.params.messageid as string;
 
   await dbConnect();
 
   const session = await getServerSession(authOptions);
-  const user: User = session?.user as User;
+  const user = session?.user as User;
 
   if (!session || !session.user) {
     return Response.json(
-      {
-        success: false,
-        message: "Unauthorized",
-      },
+      { success: false, message: "Unauthorized" },
       { status: 401 }
     );
   }
@@ -30,33 +28,24 @@ export async function DELETE(
   try {
     const updateResult = await UserModel.updateOne(
       { _id: user._id },
-      { $pull: { messages: { _id: messageId } } }
+      { $pull: { messages: { _id: messageid } } }
     );
 
     if (updateResult.modifiedCount === 0) {
       return Response.json(
-        {
-          success: false,
-          message: "Message not found or already deleted",
-        },
+        { success: false, message: "Message not found or already deleted" },
         { status: 404 }
       );
     }
 
     return Response.json(
-      {
-        success: true,
-        message: "Message deleted successfully",
-      },
+      { success: true, message: "Message deleted successfully" },
       { status: 200 }
     );
   } catch (error) {
     console.error("Error deleting message:", error);
     return Response.json(
-      {
-        success: false,
-        message: "Internal Server Error",
-      },
+      { success: false, message: "Internal Server Error" },
       { status: 500 }
     );
   }
